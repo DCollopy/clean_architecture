f1499a9566bd66f2caa170daca90695ca73bd233
DCollopy <damaris.collopy@al.infnet.edu.br>
2022-07-26 21:46:32 -0300

    Add persistence and test assured rest

--- clean-web/src/test/java/br/com/cleanweb/CleanWebApplicationTests.java
+++ clean-web/src/test/java/br/com/cleanweb/CleanWebApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CleanWebApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
